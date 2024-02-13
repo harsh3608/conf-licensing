547a01a76049d4336074592055f26b0f67a7e998
Harsh Patel <harsh3608@gmail.com>
2024-02-13 09:43:31 +0530

    ..

--- src/app/shared/Constants.ts
+++ src/app/shared/Constants.ts
@@ -1,8 +1,8 @@
 
 export class Constants{
 
-    public static baseServerUrl: string = 'https://devlicenseingapi.azurewebsites.net/api/';
-    //public static baseServerUrl: string = 'https://localhost:44328/api/';
+    // public static baseServerUrl: string = 'https://devlicenseingapi.azurewebsites.net/api/';
+    public static baseServerUrl: string = 'https://localhost:44328/api/';
 
 }
 
